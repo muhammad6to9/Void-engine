d275108d095faf5cf0d9b244e46ac314b2dacb24
muhammad6to9 <adm118331@gmail.com>
2024-08-30 03:09:10 +0300

    Voidengine.cpp
    update 0.0

---
+++ Voidengine.cpp
@@ -0,0 +1,31 @@
+/*©toMuhammad Adam 2024*/
+#include <iostream> 
+#include <cmath> 
+#include <math.h> 
+#include <string> 
+#include <random> 
+#include <typeinfo> 
+#include <vector> 
+#include <limits> 
+#include <cstdlib> 
+#include <ctime> 
+#include <unistd.h>
+#include <cstdio>
+#include <iomanip>
+#include <atomic>
+#include <thread>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+vector<char> pieces={'k','q','r','b','kn','p'};
+pieces[1]=
+
+
+
+
+
+
+}
